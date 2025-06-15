5abb41919746dbf8ea103e6467df6020c1c0af5c
mwael <mwael.y25@gmail.com>
2025-06-15 04:26:18 +0300

    production host

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     """Main entry point for the API server."""
     uvicorn.run(
         app,
-        host="127.0.0.1",
-        # host="0.0.0.0",
+        host="0.0.0.0",
         port=8000,
         log_level="info"
     )
